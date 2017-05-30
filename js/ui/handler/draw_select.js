da6d0c7e0d6e80b4208e4f5a8cdb875cd9e342fa
Gustavo <gbuzogany@gmail.com>
2017-05-30 12:40:32 +0200

    Improved multiselection.

--- js/ui/handler/draw_select.js
+++ js/ui/handler/draw_select.js
@@ -10,6 +10,7 @@ function DrawSelectHandler(map) {
     this._map = map;
     this._el = map.getCanvasContainer();
     this._container = map.getContainer();
+    this._selectionKeyPressed = false;
 
     util.bindHandlers(this);
 }
@@ -30,17 +31,38 @@ DrawSelectHandler.prototype = {
     enable: function () {
         if (this.isEnabled()) return;
         this._el.addEventListener('mousedown', this._onMouseDown, false);
+        this._el.addEventListener('keypress', this._onKeyPress, false);
         this._enabled = true;
     },
 
     disable: function () {
         if (!this.isEnabled()) return;
         this._el.removeEventListener('mousedown', this._onMouseDown);
+        this._el.removeEventListener('keypress', this._onKeyPress);
         this._enabled = false;
     },
 
+    _onKeyPress: function (e) {
+        var container = this._map.getCanvas();
+        if (e.target === container) {
+            if (e.code === 'KeyS' && this._selectionKeyPressed === false) {
+                this._selectionKeyPressed = true;
+                this._el.removeEventListener('keypress', this._onKeyPress);
+                window.document.addEventListener('keyup', this._onKeyUp, false);
+            }
+        }
+    },
+
+    _onKeyUp: function (e) {
+        if (e.code === 'KeyS' && this._selectionKeyPressed === true) {
+            this._selectionKeyPressed = false;
+            this._el.removeEventListener('keyup', this._onKeyUp);
+            this._el.addEventListener('keypress', this._onKeyPress, false);
+        }
+    },
+
     _onMouseDown: function (e) {
-        if (!(e.shiftKey && e.button === 0)) return;
+        if (!((e.shiftKey || this._selectionKeyPressed === true) && e.button === 0)) return;
 
         window.document.addEventListener('mousemove', this._onMouseMove, false);
         window.document.addEventListener('keydown', this._onKeyDown, false);
@@ -48,6 +70,7 @@ DrawSelectHandler.prototype = {
 
         DOM.disableDrag();
         this._startPos = DOM.mousePos(this._el, e);
+        this._startTime = performance.now();
         this._coordList = [this._startPos];
         this._active = true;
     },
@@ -82,39 +105,86 @@ DrawSelectHandler.prototype = {
         this._ctx.fill();
     },
 
+    _distAB: function (a, b) {
+        return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+    },
+
     _onMouseUp: function (e) {
         if (e.button !== 0) return;
 
         var source = this;
+        var endTime = performance.now();
+        var endPos = DOM.mousePos(this._el, e);
+        this._coordList.push(endPos);
+        var drawDistance = 0;
 
-        var coordsList = [];
-        var minX = 999, maxX = -999, minY = 999, maxY = -999;
-        for (var i = 0; i < this._coordList.length; i++) {
-            var point = this._map.unproject([this._coordList[i].x, this._coordList[i].y]);
-            coordsList.push({ lng: point.lng, lat: point.lat});
-            if (point.lng < minX) minX = point.lng;
-            if (point.lng > maxX) maxX = point.lng;
-            if (point.lat < minY) minY = point.lat;
-            if (point.lat > maxY) maxY = point.lat;
+        var wasClick = false;
+
+        if (this._coordList.length > 1) {
+            var last = 0;
+            for (var i = 1; i < this._coordList.length; i++) {
+                drawDistance += this._distAB(this._coordList[last], this._coordList[i]);
+                last = i;
+            }
+            if (drawDistance < 10) {
+                wasClick = true;
+            }
+        } else {
+            // if mouse barely moved, it was just a click
+            wasClick = true;
         }
 
-        var boundaries = { minX: minX, maxX: maxX, minY: minY, maxY: maxY };
+        if (endTime - this._startTime < 100) {
+            // if click was fast, it was just a click
+            wasClick = true;
+        }
 
-        this._map.mapControl.deviceManager.queryBoundaries(boundaries).then(function (result) {
-            if (result.length > 0 && result.length < 500) {
-                var markers = [];
-                for (var i = 0; i < result.length; i++) {
-                    if (source.inside(result[i], coordsList)) {
-                        markers.push(result[i]);
+        if (wasClick === true) {
+            var point = this._map.unproject([endPos.x, endPos.y]);
+            this._map.mapControl.deviceManager.queryCoords(point).then(function (result) {
+                if (result.length > 0 && result.length < 500) {
+                    for (var i = 0; i < result.length; i++) {
+                        source._map.mapControl.deviceManager.switchSelection(result[i].id);
                     }
+                    source._map.mapControl.deviceManager.applySelectionList();
+                    source._map.mapControl.deviceManager.triggerSelection();
+                    source._fireEvent('drawselectend', { markers : result});
                 }
-                source._map.mapControl.deviceManager.setSelection(markers);
-                source._map.mapControl.clickEvent.trigger([markers]);
-                source._fireEvent('drawselectend', { markers : markers});
-            }
-        });
+            });
 
-        this._finish();
+            this._finish();
+
+        } else {
+            var coordsList = [];
+            var minX = 999, maxX = -999, minY = 999, maxY = -999;
+            for (var i = 0; i < this._coordList.length; i++) {
+                var point = this._map.unproject([this._coordList[i].x, this._coordList[i].y]);
+                coordsList.push({ lng: point.lng, lat: point.lat});
+                if (point.lng < minX) minX = point.lng;
+                if (point.lng > maxX) maxX = point.lng;
+                if (point.lat < minY) minY = point.lat;
+                if (point.lat > maxY) maxY = point.lat;
+            }
+
+            var boundaries = { minX: minX, maxX: maxX, minY: minY, maxY: maxY };
+
+            this._map.mapControl.deviceManager.queryBoundaries(boundaries).then(function (result) {
+                if (result.length > 0 && result.length < 500) {
+                    var markers = [];
+                    for (var i = 0; i < result.length; i++) {
+                        if (source.inside(result[i], coordsList)) {
+                            markers.push(result[i]);
+                            source._map.mapControl.deviceManager.addToSelectionList(result[i].id);
+                        }
+                    }
+                    source._map.mapControl.deviceManager.applySelectionList();
+                    source._map.mapControl.deviceManager.triggerSelection();
+                    source._fireEvent('drawselectend', { markers : markers});
+                }
+            });
+
+            this._finish();
+        }
 
     },
     inside: function (point, shape) {
